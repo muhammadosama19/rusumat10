b350062da3920e68eaae6d63c5511716ac3fcdba
muhammadosama19 <01128613705fm@gmail.com>
2023-12-08 23:24:57 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,18 @@ export default function Home() {
   return (
     <main className="h-full">
       <section className="grid grid-cols-2 gap-5 m-5 h-[480px]">
-        <div className="mt-32">
-          <h1 className="text-4xl font-medium">
-            <h1 className="text-6xl font-bold my-5 bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-violet-800">
-              Empower
+        <div className="mt-16">
+          <div className="text-4xl font-medium">
+            <h1 className="text-6xl leading-tight font-bold my-7 bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-violet-800">
+              Empower, business, API
             </h1>
-            your business with API from Flaticon
-          </h1>
+            <h1>your business with API from Rusumat</h1>
+            <h1 className="text-lg my-5 font-normal">your business with API from Rusumat your business with API from Rusumat your business with API from Rusumat</h1>
+          </div>
         </div>
-        <div className="mt-32"></div>
+        <div className="mt-16">
+          
+        </div>
       </section>
 
       <section className="grid grid-cols-3 gap-5 m-10 h-96">
